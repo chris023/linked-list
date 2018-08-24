85f3ae8d1e894d38faa574436373733c63e4e6bd
Jamie Rushford <jar.rushford@gmail.com>
2018-08-24 13:05:56 -0600

    Add list creation and item adding function

--- js/main.js
+++ js/main.js
@@ -2,19 +2,38 @@ const titleInput = document.querySelector('.title-input');
 const urlInput = document.querySelector('.url-input');
 const enterButton = document.querySelector('.enter-button');
 const listSection = document.querySelector('.list-section');
+let readButtonArray = [];
+let deleteButtonArray = [];
+let bookmarksHTMLArray = [];
 
 enterButton.addEventListener('click', function(){
   newTitle = titleInput.value;
   newUrl = urlInput.value;
-  createNewBookmark(newTitle, newUrl);
+  createNewBookmarkHTML(newTitle, newUrl);
+  createListHTML(bookmarksHTMLArray);
+  createReadListeners();
   event.preventDefault();
 });
 
-function createNewBookmark(newTitle, newUrl){
-  listSection.innerHTML += `<article class='bookmark'>
+function createBookmarkHTML(newTitle, newUrl){
+  bookmarksHTMLArray.push(`<article class='bookmark'>
   <h1 class='bookmark-title'>${newTitle}</h1>
   <a class='bookmark-url'>${newUrl}</a>
   <p class='bookmark-read'>Read</p>
   <p class='bookmark-delete'>Delete</p>
-</article>`;
+  </article>`);
 }
+
+function createListHTML(arr) {
+  booksmarksString = '';
+  for (var i = 0; i < bookmarksHTMLArray.length; i++) {
+    booksmarksString += bookmarksHTMLArray[i];
+  }
+  listSection.innerHTML = booksmarksString;
+}
+
+function createReadListeners()
+
+
+
+query selector all for read buttons
