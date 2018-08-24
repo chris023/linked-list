2e4940eda0d7fd7cd3d882dcb1938f2b29753e8a
Jamie Rushford <jar.rushford@gmail.com>
2018-08-24 17:03:02 -0600

    Add read and delete listeners, still not working

--- js/main.js
+++ js/main.js
@@ -9,9 +9,10 @@ let bookmarksHTMLArray = [];
 enterButton.addEventListener('click', function(){
   newTitle = titleInput.value;
   newUrl = urlInput.value;
-  createNewBookmarkHTML(newTitle, newUrl);
+  createBookmarkHTML(newTitle, newUrl);
   createListHTML(bookmarksHTMLArray);
   createReadListeners();
+  createDeleteListeners();
   event.preventDefault();
 });
 
@@ -26,14 +27,32 @@ function createBookmarkHTML(newTitle, newUrl){
 
 function createListHTML(arr) {
   booksmarksString = '';
-  for (var i = 0; i < bookmarksHTMLArray.length; i++) {
+  for (let i = 0; i < bookmarksHTMLArray.length; i++) {
     booksmarksString += bookmarksHTMLArray[i];
   }
   listSection.innerHTML = booksmarksString;
 }
 
-function createReadListeners()
+function createReadListeners() {
+  let readButton = document.querySelectorAll('.bookmark-read')[readButtonArray.length];
+  readButtonArray.push(readButton);
+  readButton.addEventListener('click', function() {
+    readButton.classList.toggle('read');
+  });
+}
 
+function createDeleteListeners() {
+  let deleteButton = document.querySelectorAll('.bookmark-delete')[deleteButtonArray.length];
+  deleteButtonArray.push(deleteButton);
+  let currentArrayLength = deleteButtonArray.length;
+  deleteButtonArray[currentArrayLength - 1].addEventListener('click', function(currentArrayLength) {
+    let index = deleteButtonArray.findIndex(function(element) {
+      return element === deleteButton;
+    });
+    bookmarksHTMLArray.splice(index, 1);
+    readButtonArray.splice(index, 1);
+    deleteButtonArray.splice(index, 1);
+    createListHTML(bookmarksHTMLArray);
+  });
+}
 
-
-query selector all for read buttons
