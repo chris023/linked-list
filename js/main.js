5980d229ed42bccbcdd3764be03884e3cb31a401
Jamie Rushford <jar.rushford@gmail.com>
2018-08-26 09:16:40 -0600

    Fix read listeners using IDs
    Code is still a bit of a mess, but it is now working to add a new article to the HTML and create a read listener for that specific card at that time

--- js/main.js
+++ js/main.js
@@ -4,7 +4,8 @@ const enterButton = document.querySelector('.enter-button');
 const listSection = document.querySelector('.list-section');
 let readButtonArray = [];
 let deleteButtonArray = [];
-let bookmarksHTMLArray = [];
+let articleId = 0;
+// let bookmarksHTMLArray = [];
 
 titleInput.addEventListener('input', function() {
   if (!!titleInput.value) {
@@ -20,52 +21,72 @@ urlInput.addEventListener('input', function() {
   enterButton.disabled = true;
 });
 
-enterButton.addEventListener('click', function(){
+enterButton.addEventListener('click', function(event){
   newTitle = titleInput.value;
   newUrl = urlInput.value;
   if (!newTitle || !newUrl) {
     alert('Please enter a Title and URL for your new bookmark!');
   } else
   createBookmarkHTML(newTitle, newUrl);
-  createListHTML(bookmarksHTMLArray);
-  createReadListeners();
-  createDeleteListeners();
+  // createListHTML(bookmarksHTMLArray);
+  // createReadListeners();
+  // createDeleteListeners();
   event.preventDefault();
 });
 
-function createBookmarkHTML(newTitle, newUrl){
-  bookmarksHTMLArray.push(
-    `<article class='bookmark'>
+// function createBookmarkHTML(newTitle, newUrl){
+//   bookmarksHTMLArray.push(
+//     `<article class='bookmark'>
+//       <h1 class='bookmark-title'>${newTitle}</h1>
+//       <a class='bookmark-url css-links' href="${newUrl}">${newUrl}</a>
+//       <p class='bookmark-read css-links css-read-and-delete'>Read</p>
+//       <p class='bookmark-delete css-links css-read-and-delete'>Delete</p>
+//     </article>`);
+// }
+
+function createBookmarkHTML(newTitle, newUrl) {
+  listSection.insertAdjacentHTML('beforeend', `<article class='bookmark' id='bookmark${articleId}'>
       <h1 class='bookmark-title'>${newTitle}</h1>
       <a class='bookmark-url css-links' href="${newUrl}">${newUrl}</a>
-      <p class='bookmark-read css-links css-read-and-delete'>Read</p>
+      <p class='bookmark-read css-links css-read-and-delete' id='read${articleId}'>Read</p>
       <p class='bookmark-delete css-links css-read-and-delete'>Delete</p>
     </article>`);
+  createReadListener();
+  articleId++;
 }
 
-function createListHTML(arr) {
-  booksmarksString = '';
-  for (let i = 0; i < bookmarksHTMLArray.length; i++) {
-    booksmarksString += bookmarksHTMLArray[i];
-  }
-  listSection.innerHTML = booksmarksString;
-}
+// function createListHTML(arr) {
+//   booksmarksString = '';
+//   for (let i = 0; i < bookmarksHTMLArray.length; i++) {
+//     booksmarksString += bookmarksHTMLArray[i];
+//   }
+//   listSection.innerHTML = booksmarksString;
+// }
 
-function createReadListeners() {
-  readButtonArray = document.querySelectorAll('.bookmark-read');
-  for (let i = 0; i < readButtonArray.length; i++) {
-    readButtonArray[i].addEventListener('click', function() { 
-      document.querySelectorAll('.bookmark')[i].classList.toggle('read');
-    });
-  }
+// function createReadListeners() {
+//   readButtonArray = document.querySelectorAll('.bookmark-read');
+//   for (let i = 0; i < readButtonArray.length; i++) {
+//     readButtonArray[i].addEventListener('click', function() { 
+//       document.querySelectorAll('.bookmark')[i].classList.toggle('read');
+//     });
+//   }
+// }
+
+function createReadListener() {
+  let readButton = document.querySelector(`#read${articleId}`);
+  let bookmark = document.querySelector(`#bookmark${articleId}`);
+  readButton.addEventListener('click', function() {
+    bookmark.classList.toggle('read');
+  });
 }
 
 function createDeleteListeners() {
+  let articles = document.querySelectorAll('.bookmark');
   deleteButtonArray = document.querySelectorAll('.bookmark-delete');
   for (let i = 0; i < deleteButtonArray.length; i++) {
-    deleteButtonArray[i].addEventListener('click', function() {
-      bookmarksHTMLArray.splice(i, 1);
-      createListHTML(bookmarksHTMLArray);
+    deleteButtonArray[i].addEventListener('click', function() { 
+      // bookmarksHTMLArray.splice(i, 1);
+      // createListHTML(bookmarksHTMLArray);
       createReadListeners();
       createDeleteListeners();
     });
