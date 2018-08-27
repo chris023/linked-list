bba854c13439805affe80313701b5cba07fc977d
Jamie Rushford <jar.rushford@gmail.com>
2018-08-27 09:09:10 -0600

    Delete old code

--- js/main.js
+++ js/main.js
@@ -28,18 +28,9 @@ enterButton.addEventListener('click', function(event){
   event.preventDefault();
 });
 
-// function createBookmarkHTML(newTitle, newUrl){
-//   bookmarksHTMLArray.push(
-//     `<article class='bookmark'>
-//       <h1 class='bookmark-title'>${newTitle}</h1>
-//       <a class='bookmark-url css-links' href="${newUrl}">${newUrl}</a>
-//       <p class='bookmark-read css-links css-read-and-delete'>Read</p>
-//       <p class='bookmark-delete css-links css-read-and-delete'>Delete</p>
-//     </article>`);
-// }
-
 function createBookmarkHTML(newTitle, newUrl) {
-  listSection.insertAdjacentHTML('beforeend', `<article class='bookmark' id='bookmark${articleId}'>
+  listSection.insertAdjacentHTML('beforeend', 
+    `<article class='bookmark' id='bookmark${articleId}'>
       <h1 class='bookmark-title'>${newTitle}</h1>
       <a class='bookmark-url css-links' href="${newUrl}">${newUrl}</a>
       <p class='bookmark-read css-links css-read-and-delete' id='read${articleId}'>Read</p>
@@ -49,23 +40,6 @@ function createBookmarkHTML(newTitle, newUrl) {
   createDeleteListener();
   articleId++;
 }
-
-// function createListHTML(arr) {
-//   booksmarksString = '';
-//   for (let i = 0; i < bookmarksHTMLArray.length; i++) {
-//     booksmarksString += bookmarksHTMLArray[i];
-//   }
-//   listSection.innerHTML = booksmarksString;
-// }
-
-// function createReadListeners() {
-//   readButtonArray = document.querySelectorAll('.bookmark-read');
-//   for (let i = 0; i < readButtonArray.length; i++) {
-//     readButtonArray[i].addEventListener('click', function() { 
-//       document.querySelectorAll('.bookmark')[i].classList.toggle('read');
-//     });
-//   }
-// }
 
 function createReadListener() {
   let readButton = document.querySelector(`#read${articleId}`);
@@ -83,16 +57,4 @@ function createDeleteListener() {
   });
 }
 
-// function createDeleteListeners() {
-//   let articles = document.querySelectorAll('.bookmark');
-//   deleteButtonArray = document.querySelectorAll('.bookmark-delete');
-//   for (let i = 0; i < deleteButtonArray.length; i++) {
-//     deleteButtonArray[i].addEventListener('click', function() { 
-//       // bookmarksHTMLArray.splice(i, 1);
-//       // createListHTML(bookmarksHTMLArray);
-//       createReadListeners();
-//       createDeleteListeners();
-//     });
-//   }
-// }
 
